e38f338d1aa4dac8dc0b0c40146ef88f5b70ab02
Zen T <ztang230@gmail.com>
2024-12-08 16:57:42 -0600

    update the background

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+export default function About() {
+    return (
+        <div className="mt-16 px-6 bg-white rounded-lg py-10 max-w-4xl mx-auto">
+            <h1 className="text-4xl font-bold mb-6">Jess - 2024</h1>
+            <p className="text-2xl leading-relaxed">
+                Have you ever wanted to find someone really good to date? Who gets the same jokes you do?
+                Imagine sharing laughs and experiences with someone who truly understands you.
+            </p>
+            <p className="text-xl mt-8 text-gray-600">
+                This is an experiment in using conversational AI to match you with your perfect partner. Jess will learn from your responses to generate a profile of you, and also a list of people who you might be interested in.
+            </p>
+            <p className="text-xl mt-8 text-gray-600">
+                Or no one! This is just for fun.
+            </p>
+            <p className="text-xl mt-8 text-gray-600">
+                P.S. Your data will never be shared. We'll delete it all after a few days.
+            </p>
+        </div>
+    );
+}
